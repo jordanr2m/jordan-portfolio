c2ab455b2407ff3170c69cb00321fd6b71c774f8
Jordan J <jordanr2m@gmail.com>
2024-04-03 16:52:23 -0700

    Finished adding replay button styles.

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ const replayButton = document.querySelector("#replay");
 function wait(ms = 0) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-  
+
 function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
     return Math.floor(randomNumber * (max - min) + min);
 }
 
 async function draw(el) {
-    replayButton.style.opacity = 0;
+    replayButton.style.opacity = 0; // reset to 0 for each run
     const text = el.textContent;
     let soFar = '';
     for (const letter of text) {
-      soFar += letter;
-      el.textContent = soFar;
-      // wait for some amount of time before restarting loop
-      const { typeMin, typeMax } = el.dataset;
-      const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
-      await wait(amountOfTimeToWait);
+        soFar += letter;
+        el.textContent = soFar;
+        // wait for some amount of time before restarting loop
+        const { typeMin, typeMax } = el.dataset;
+        const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+        await wait(amountOfTimeToWait);
     }
     replayButton.style.opacity = 1;
 }
